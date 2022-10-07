87cf2fc48c9b6604fe12424f2f302535570b2e52
Zach Quinn <zacharytq@gmail.com>
2022-10-07 00:47:03 -0400

    back to work

--- init.lua
+++ init.lua
@@ -26,9 +26,11 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
+
+  use {
+    "kyazdani42/nvim-web-devicons",
+    module = "nvim-web-devicons"
+  }
 
   use {
     'nvim-lua/plenary.nvim',
